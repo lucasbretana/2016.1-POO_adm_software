36626e3aa79120753ac57aea09027f7f0ec982ff
Lucas E. Bretana <lucasbretana@gmail.com>
2016-06-16 13:54:30 -0300

    Organizing the projects folders

---
+++ src/tests/Main.java
@@ -0,0 +1,13 @@
+package tests;
+
+/**
+ * Used only to run some tests, do not implement real code in here
+ * */
+public class Main{
+
+	/**
+	 * Stupid Flandes, I meand, main
+	 * */
+	public static void main(String []args){
+	}
+}
