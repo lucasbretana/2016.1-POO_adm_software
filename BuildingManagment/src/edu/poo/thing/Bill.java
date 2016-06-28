742db7a7dc9bb1b40d6805fe595eb5573569e3f4
Lucas E. Bretana <lucasbretana@gmail.com>
2016-06-28 15:22:26 -0300

    Adding files

--- BuildingManagment/src/edu/poo/thing/Bill.java
+++ BuildingManagment/src/edu/poo/thing/Bill.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.BufferedReader;
 
 public class Bill implements IOBill{
-
   private String month;
   private Float water;
   private Float energy;
@@ -20,7 +19,7 @@ public class Bill implements IOBill{
  * Constructor method, creates a bill using the information from a file
  * @method Bill
  * @param  desc is the file that descrives the bill it self
- * @return      an instance  os Bill
+ * @return   an instance  os Bill
  * @throws IllegalArgumentException when the file that decrive the Bill is not valid
  */
   public Bill(File desc) throws IllegalArgumentException{
@@ -49,20 +48,135 @@ public class Bill implements IOBill{
 
       in.reset();
     }catch(FileNotFoundException fileEx){
-      throw new IllegalArgumentException("There was come problem locating the file (" + desc.getName() + ") on " + desc.getPath() + "!\n" + fileEx.getStackTrace().toString());
+      throw new IllegalArgumentException("There was a problem locating the file (" + desc.getName() + ") on " + desc.getPath() + "!\n" + fileEx.getStackTrace().toString());
       // fileEx.printStackTrace();
     }catch(IOException ioEx){
-      throw new IllegalArgumentException("There was come problem locating the file (" + desc.getName() + ") on " + desc.getPath() + "!\n" + fileEx.getStackTrace().toString());
+      throw new IllegalArgumentException("There was a problem reading the file (" + desc.getName() + ")!\n" + ioEx.getStackTrace().toString());
       // ioEx.printStackTrace();
     }catch(NumberFormatException numEx){
-      numEx.printStackTrace();
+      throw new IllegalArgumentException("There was a problem with the format of some of the information in the file (" + desc.getName() + ")!\n" + numEx.getStackTrace().toString());
+      // numEx.printStackTrace();
     }finally{
       line = null;
       System.gc();
     }
   }
 
+  /**
+   * Gets the month from memory
+   * @method getMonth
+   * @return the month if this bill
+   */
   public String getMonth(){
     return this.month;
   }
+  /**
+   * Change the month name
+   * @method setMonth
+   * @param  m is the new name of the month
+   * @IllegalArgumentException if the new month name is not valid
+   */
+  public void setMonth(String m) throws IllegalArgumentException{
+    if(m.trim().equalsIgnoreCase("")){
+      throw new IllegalArgumentException("The month value cannot be null!");
+    }
+    this.month = m;
+  }
+  /**
+   * Returns the water value
+   * @method getWater
+   * @return [description]
+   */
+  public Float getWater(){
+    return this.water;
+  }
+
+  /**
+   *
+   * @method getEnergy
+   * @return [description]
+   */
+  public Float getEnergy(){
+    return this.energy;
+  }
+
+	/**
+	* Sets new value of water
+	* @param
+	*/
+	public void setWater(Float water) {
+		this.water = water;
+	}
+
+	/**
+	* Sets new value of energy
+	* @param
+	*/
+	public void setEnergy(Float energy) {
+		this.energy = energy;
+	}
+
+	/**
+	* Returns value of gas
+	* @return
+	*/
+	public Float getGas() {
+		return gas;
+	}
+
+	/**
+	* Sets new value of gas
+	* @param
+	*/
+	public void setGas(Float gas) {
+		this.gas = gas;
+	}
+
+	/**
+	* Returns value of lobby
+	* @return
+	*/
+	public Float getLobby() {
+		return lobby;
+	}
+
+	/**
+	* Sets new value of lobby
+	* @param
+	*/
+	public void setLobby(Float lobby) {
+		this.lobby = lobby;
+	}
+
+	/**
+	* Returns value of admin
+	* @return
+	*/
+	public Float getAdmin() {
+		return admin;
+	}
+
+	/**
+	* Sets new value of admin
+	* @param
+	*/
+	public void setAdmin(Float admin) {
+		this.admin = new Double((this.water + this.energy + this.gas + this.lobby) * 0.1).floatValue();
+	}
+
+	/**
+	* Returns value of description
+	* @return
+	*/
+	public File getDescription() {
+		return description;
+	}
+
+	/**
+	* Sets new value of description
+	* @param
+	*/
+	public void setDescription(File description) {
+		this.description = description;
+	}
 }
--- BuildingManagment/src/edu/poo/thing/Resident.java
+++ BuildingManagment/src/edu/poo/thing/Resident.java
@@ -7,7 +7,7 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Resident implements IOResident{
-  private Integer numAp;
+  private Integer apartment;
   private String name;
   private Boolean garage;
   private File description = null;
@@ -28,7 +28,7 @@ public class Resident implements IOResident{
       in.mark(1);
       line = in.readLine();
       line = line.replaceFirst(IOResident.APARTMENT, " ");
-      this.numAp = Integer.parseInt(line.trim());
+      this.apartment = Integer.parseInt(line.trim());
 
 
       line = in.readLine();
@@ -54,18 +54,19 @@ public class Resident implements IOResident{
     }
   }
 
-  public Integer getApartment(){return this.numAp;}
-  public String getName(){return this.name;}
-  public Boolean hasGarage(){return this.garage;}
-  public void setApartment(Integer ap){this.numAp = ap;}
-  public void setName(String n) throws IllegalArgumentException{
-    if(n.equalsIgnoreCase("")){
-      throw new IllegalArgumentException("The name cannot be changed to null!");
-    }
-    this.name = n;
-  }
-  public void setGarage(Boolean g){this.garage = g;}
+	/**
+	* Returns value of apartment
+	* @return
+	*/
+	public Integer getApartment() {
+		return apartment;
+	}
 
+  /**
+   * Returns the value of apartment that is in the file
+   * @method getApartmentFromFile
+   * @return [description]
+   */
   public Integer getApartmentFromFile(){
     String line;
     try(BufferedReader in = new BufferedReader(new FileReader(this.description))){
@@ -73,7 +74,7 @@ public class Resident implements IOResident{
 
       line = in.readLine();
       line = line.replaceFirst(IOResident.APARTMENT, " ");
-      this.numAp = Integer.parseInt(line.trim());
+      this.apartment = Integer.parseInt(line.trim());
 
       in.reset();
     }catch(FileNotFoundException fileExc){
@@ -88,8 +89,31 @@ public class Resident implements IOResident{
       line = null;
       System.gc();
     }
-    return this.numAp;
+    return this.apartment;
   }
+
+
+	/**
+	* Sets new value of apartment
+	* @param
+	*/
+	public void setApartment(Integer apartment) {
+		this.apartment = apartment;
+	}
+
+	/**
+	* Returns value of name
+	* @return
+	*/
+	public String getName() {
+		return name;
+	}
+
+  /**
+   * Look in the file for the name of the resident and reload
+   * @method getNameFromFile
+   * @return the name of the resident
+   */
   public String getNameFromFile(){
     String line = null;
     try(BufferedReader in = new BufferedReader(new FileReader(this.description))){
@@ -116,6 +140,30 @@ public class Resident implements IOResident{
     return this.name;
   }
 
+	/**
+	* Sets new value of name
+	* @param n is the new name
+	*/
+  public void setName(String n) throws IllegalArgumentException{
+    if(n.equalsIgnoreCase("")){
+      throw new IllegalArgumentException("The name cannot be changed to null!");
+    }
+    this.name = n;
+  }
+
+	/**
+	* Returns value of garage
+	* @return
+	*/
+	public Boolean hasGarage() {
+		return garage;
+	}
+
+  /**
+   * Returns value of garaga, that is in the file
+   * @method hasGarageFromFile
+   * @return the boolean information saying if this resident has a garage
+   */
   public Boolean hasGarageFromFile(){
     String line;
     try(BufferedReader in = new BufferedReader(new FileReader(this.description))){
@@ -144,4 +192,27 @@ public class Resident implements IOResident{
     return this.garage;
   }
 
+	/**
+	* Sets new value of garage
+	* @param
+	*/
+	public void setGarage(Boolean garage) {
+		this.garage = garage;
+	}
+
+	/**
+	* Returns value of description
+	* @return
+	*/
+	public File getDescription() {
+		return description;
+	}
+
+	/**
+	* Sets new value of description
+	* @param
+	*/
+	public void setDescription(File description) {
+		this.description = description;
+	}
 }
