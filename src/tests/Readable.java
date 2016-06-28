5bcf0b9dec01763898c505d8db3c40cc5dab9aef
Lucas E. Bretana <lucasbretana@gmail.com>
2016-06-28 00:33:04 -0300

    Removing, and restarting

--- src/tests/Readable.java
+++
@@ -1,7 +0,0 @@
-package tests;
-
-public interface Readable{
-  public static final String AP = "Apartamento: ";
-  public static final String NOM = "Nome do Proprietário: ";
-  public static final String GAR = "Garagem: ";
-}
